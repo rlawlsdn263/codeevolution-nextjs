0148c82b1ff8c8d6fd24fe3ba15075881d07a5a4
rlawlsdn263 <rlawlsdn263@naver.com>
2024-01-15 12:54:52 +0900

    [✍학습] 35. Handling POST Request
    라우트 핸들로는 `request` 파라미터를 가진다. 이 파라미터를 사용해 요청값으로 들어온 데이터에 접근할 수 있다.
    
    접근하려는 `request` 데이터는 `request.json()`을 사용해 JSON 형식으로 변환을 해준다.
    
    새로운 댓글이 추가될 수 있도록 `newComment` 객체를 만들고, `comments` 배열에 추가를 해줬다.
    
    `return`에는 응답값이 들어가야 하기 때문에 `new Response`로 객체를 만들어주고, 새로 추가된 `newComment`와 `headers`의 타입을 정해줬다. 이때, POST 통신이기 때문에 `status`는 200이 아니라 201을 넣었다.

--- src/app/comments/route.ts
+++ src/app/comments/route.ts
@@ -3,3 +3,24 @@ import { comments } from "./data";
 export async function GET() {
   return Response.json(comments);
 }
+
+// 라우트 핸들러는 request 파라미터로 들어온 값을 받는다
+export async function POST(request: Request) {
+  // 요청받은 데이터 저장
+  const comment = await request.json();
+
+  // 들어온 데이터 객체로 만들어서 commets DB에 집어넣기
+  const newComment = {
+    id: comments.length + 1,
+    text: comment.text,
+  };
+  comments.push(newComment);
+
+  // 응답 객체 생하고 JSON 형식으로 반환한 다음 headers 설정
+  return new Response(JSON.stringify(newComment), {
+    headers: {
+      "Content-Type": "application/json",
+    },
+    status: 201,
+  });
+}
